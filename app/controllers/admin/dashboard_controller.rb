d7ecca63a26760b0fd75d83e3035d0eab242628b
redjazz96 <redjazz96@gmail.com>
2012-12-12 17:52:26 -0500

    Updates Admin Dashboard

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -5,7 +5,15 @@ class Admin::DashboardController < ApplicationController
     current = Time.now
     future = Time.new current.year, current.month, current.day, current.hour
     @post_history = Rails.cache.fetch(:post_history, :expires => future) do
-      Post.where("created_at > ?", 24.hours.ago).chunk { |post| post.created_at.hour }.map { |h,ps| [h, ps.count] }
+      count = Post.where("created_at > ?", 24.hours.ago).chunk { |post| post.created_at.hour }.map { |h,ps| [h, ps.count] }
+      h = {}
+      count.each do |c|
+        h[c[0]] = c[1]
+      end
+      24.times do |i|
+        h[i] ||= 0
+      end
+      h.sort
     end
 	end
 end
