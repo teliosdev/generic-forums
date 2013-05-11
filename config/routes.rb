3c09b1cc0c1b95f5bd657aace6ad1136ef2e8ed0
redjazz96 <redjazz96@gmail.com>
2013-05-11 18:20:34 -0400

    Adds Home Controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ GenericForums::Application.routes.draw do
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root to: 'home#index'
 
   # You can have the root of your site routed with "root"
   # root to: 'welcome#index'
