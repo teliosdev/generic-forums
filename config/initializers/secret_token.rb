ceeea22d1f9ef7fe19a2859f332ad8e20e10e493
redjazz96 <redjazz96@gmail.com>
2013-05-11 16:23:10 -0400

    Updates Rails Version

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,11 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
+
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-GenericForums::Application.config.secret_token = '3ccfa2798df80eab9a1acd8ff330c3b29a9be0ffae3f5fe6e768a99767517e0b8ea2ee6cd5bca0e83fac90c650f48f97b61a5dbf7dd4f7e10a371d44fc0f6329'
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GenericForums::Application.config.secret_key_base = 'ff7053bc71cb22bf8d18264313b05277f25e9431dc8ae87be8069c874fc4631b42043a2a391cad669c746b510d7f1a167e938c906f7688d70f07b19dae302199'
