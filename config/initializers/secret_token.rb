248eb801a6eded75027194815c0aa6af5e8b655d
redjazz96 <redjazz96@gmail.com>
2013-05-11 15:57:18 -0400

    Adds Rails Files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+GenericForums::Application.config.secret_token = '3ccfa2798df80eab9a1acd8ff330c3b29a9be0ffae3f5fe6e768a99767517e0b8ea2ee6cd5bca0e83fac90c650f48f97b61a5dbf7dd4f7e10a371d44fc0f6329'
