49135516abc7a6685081a9cb6ade0da20a45d9f8
redjazz96 <redjazz96@gmail.com>
2013-10-23 16:48:13 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GenericForums::Application.config.secret_key_base = 'a3d29ef4c60ed9e2b40f5ec99b3317e826a946a17e14c3d0cdee8bc6780b69879e15b88f1b86e230ee04cebe3dc2edfa62840c0ea07f4e85b3b3194c3814c580'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GenericForums::Application.config.session_store :cookie_store, key: '_generic-forums_session'
